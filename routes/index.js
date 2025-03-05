b64ea24c22e6419989997eec581c44490adcfc09
Mich827 <100875018+Mich827@users.noreply.github.com>
2025-03-05 22:19:32 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 router.get("/articles", (req, res) => {
   fetch(
-    `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${NEWS_API_KEY}`
+    `https://newsapi.org/v2/everything?sources=the-verge&apiKey=${NEWS_API_KEY}`
   )
     .then((response) => response.json())
     .then((data) => {
