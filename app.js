8c9a729503c958762ffb9d4ef12d71c3f73215e3
Mich827 <100875018+Mich827@users.noreply.github.com>
2025-03-09 20:41:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,13 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://morning-news-frontend-xi.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
