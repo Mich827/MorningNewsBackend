532fccab0c3afc490699d7e434a289f68a169eab
Mich827 <100875018+Mich827@users.noreply.github.com>
2025-03-09 20:34:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,20 +10,21 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-//const cors = require('cors');
-//app.use(cors());
-/*app.use(cors({
-  origin: 'https://morning-news-frontend-xi.vercel.app', // Autoriser les requêtes depuis cette origine
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Autoriser ces méthodes
-  allowedHeaders: ['Content-Type', 'Authorization'], // Autoriser ces en-têtes
-  preflightContinue: false, // Passer la réponse CORS préalable à l'handler suivant
-}));*/
+const cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://morning-news-frontend-xi.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
