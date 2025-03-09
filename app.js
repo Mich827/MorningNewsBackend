3d702af0f3b332a1b6e0851490ac0e6728bb170b
Mich827 <100875018+Mich827@users.noreply.github.com>
2025-03-09 19:19:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const cors = require('cors');
+//const cors = require('cors');
 //app.use(cors());
-app.use(cors({
+/*app.use(cors({
   origin: 'https://morning-news-frontend-xi.vercel.app', // Autoriser les requêtes depuis cette origine
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Autoriser ces méthodes
   allowedHeaders: ['Content-Type', 'Authorization'], // Autoriser ces en-têtes
   preflightContinue: false, // Passer la réponse CORS préalable à l'handler suivant
-}));
+}));*/
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
